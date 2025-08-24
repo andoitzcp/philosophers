1d8ba186c6801f89ea078386e96a4a308285e188
Andoitz Campo Perez <acampo-p@c4r2s2.42urduliz.com>
2025-08-24 16:13:45 +0200

    fix: fork mutex unlocked when philo dies

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: acampo-p@student.42urduliz.com <marvi      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 14:06:43 by acampo-p@         #+#    #+#             */
-/*   Updated: 2025/08/24 14:58:18 by acampo-p@        ###   ########.fr       */
+/*   Updated: 2025/08/24 16:10:07 by acampo-p@        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,22 +38,19 @@ unsigned long	get_tstamp_usecs(t_tstamp *tstamp)
 	return (usecs);
 }
 
-void	*routine(void *p)
+void	run_routine_loop(t_philo *philo)
 {
-	t_philo		*philo;
-	t_params	*params;
-
-	philo = (t_philo *)p;
-	params = philo->prompt->params;
-	action_think(philo);
-	usleep(params->ttd * philo->nbr / 100);
 	while (is_philo_finished_eating(philo->prompt, philo) == 0)
 	{
 		if (get_death_flag(philo->prompt) == 1)
 			break ;
 		action_take_forks(philo);
 		if (get_death_flag(philo->prompt) == 1)
+		{
+			pthread_mutex_unlock(philo->lfm);
+			pthread_mutex_unlock(philo->rfm);
 			break ;
+		}
 		action_eat(philo);
 		if (get_death_flag(philo->prompt) == 1)
 			break ;
@@ -64,5 +61,18 @@ void	*routine(void *p)
 		if (get_death_flag(philo->prompt) == 1)
 			break ;
 	}
+	return ;
+}
+
+void	*routine(void *p)
+{
+	t_philo		*philo;
+	t_params	*params;
+
+	philo = (t_philo *)p;
+	params = philo->prompt->params;
+	action_think(philo);
+	usleep(params->ttd * philo->nbr / 100);
+	run_routine_loop(philo);
 	return (p);
 }
